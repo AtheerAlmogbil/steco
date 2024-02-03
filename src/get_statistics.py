db9f2b3996c0a7a25052e11a87cb70fd4ff23ef6
AtheerAlmogbil <68163999+AtheerAlmogbil@users.noreply.github.com>
2024-02-03 08:49:02 -0500

    Update get_statistics.py

--- src/get_statistics.py
+++ src/get_statistics.py
@@ -97,71 +97,7 @@ class Summary:
         return df
 
 
-def get_text_statistics(settings: Settings = text_default_settings, n_examples: int = 100, save_data: bool = False) -> None:
-    if settings.algo == 'sample':
-        from random_sample_cy import encode_text
-    elif settings.algo in ['Discop', 'Discop_baseline']:
-        from stega_cy import encode_text
-    else:
-        raise NotImplementedError("`Settings.algo` must belong to {'Discop', 'Discop_baseline', 'sample'}!")
 
-    model = get_model(settings)
-    tokenizer = get_tokenizer(settings)
-
-    dataset = load_dataset(text_context_dataset, split='train')[:n_examples]['text']
-
-    time_stamp = time.strftime("%m%d_%H%M", time.localtime())
-
-    if save_data:
-        save_data_main_dir = os.path.join(
-            'data', settings.task,
-            '{}_{}_{}'.format(settings.model_name.split('/')[-1],
-                              str(settings.device).replace(':', '-'), time_stamp))
-        check_dir(save_data_main_dir)
-
-    df = pd.DataFrame(columns=summary_columns)
-    for top_p in top_p_lst:
-        settings.top_p = top_p
-        summary = Summary(settings)
-
-        if save_data:
-            save_context_path = os.path.join(save_data_main_dir, '{:.2f}_context.txt'.format(top_p))
-            save_stego_path = os.path.join(save_data_main_dir, '{:.2f}_stego.txt'.format(top_p))
-            f_context = open(save_context_path, 'w')
-            f_stego = open(save_stego_path, 'w')
-            context_lst = []
-            stego_lst = []
-
-        for i in tqdm(range(n_examples), ncols=70, desc='p={:.2f}'.format(top_p)):
-            random.seed(os.urandom(1))
-            message_start_index = random.randint(0, 10000)
-
-            context = dataset[i]
-            context = context.replace('<br /><br />', ' ').replace('<br />', ' ')  # remove all '<br />'
-            context = ' '.join(sent_tokenize(context)[:3])  # Selecting leading 3 sentences as `context`
-            settings.seed = os.urandom(1)
-            example = encode_text(model, tokenizer, message[message_start_index:], context, settings)
-            summary.add_example(example)
-            if save_data:
-                context_lst.append(context)
-                stego_lst.append(example.stego_object)
-        if save_data:
-            f_context.write('\n'.join(context_lst))
-            f_stego.write('\n'.join(stego_lst))
-
-        print(summary)
-        print()
-        df = pd.concat([df, summary.gather()], ignore_index=True)
-    save_table_dir = os.path.join('results', settings.task)
-    check_dir(save_table_dir)
-    save_table_filename = '{}_{}_{}.xlsx'.format(
-        settings.model_name.split('/')[-1],
-        str(settings.device).replace(':', '-'), time_stamp)
-    save_table_path = os.path.join(save_table_dir, save_table_filename)
-    df.to_excel(save_table_path)
-    if save_data:
-        f_context.close()
-        f_stego.close()
 
 
 def get_image_statistics(settings: Settings = image_default_settings,
@@ -233,80 +169,10 @@ def get_image_statistics(settings: Settings = image_default_settings,
     df.to_excel(save_table_path)
 
 
-def get_audio_statistics(settings: Settings = audio_default_settings, n_examples: int = 30, save_data: bool = False) -> None:
-    from stega_tts import get_tts_model
-    if settings.algo == 'sample':
-        from stega_tts import random_sample_speech as encode_speech
-    elif settings.algo in ['Discop', 'Discop_baseline']:
-        from stega_tts import encode_speech
-    else:
-        raise NotImplementedError("`Settings.algo` must belong to {'Discop', 'Discop_baseline', 'sample'}!")
-
-    vocoder, tacotron, cmudict = get_tts_model(settings)
-
-    dataset = load_dataset(tts_text_dataset, split='train')[:n_examples]['text']
-
-    time_stamp = time.strftime("%m%d_%H%M", time.localtime())
-
-    if save_data:
-        save_data_main_dir = os.path.join(
-            'data', settings.task,
-            '{}_{}_{}'.format(settings.model_name.split('/')[-1],
-                              str(settings.device).replace(':', '-'), time_stamp))
-        check_dir(save_data_main_dir)
-
-    df = pd.DataFrame(columns=summary_columns)
-    for top_p in top_p_lst:
-        settings.top_p = top_p
-
-        summary = Summary(settings)
-
-        if save_data:
-            save_data_sub_dir = os.path.join(save_data_main_dir, '{:.2f}'.format(top_p))
-            check_dir(save_data_sub_dir)
-            save_text_path = os.path.join(save_data_main_dir, '{:.2f}.txt'.format(top_p))
-            text_lst = []
-            f_text = open(save_text_path, 'w')
-
-        for i in tqdm(range(n_examples), ncols=70, desc='p={:.2f}'.format(top_p)):
-            random.seed(os.urandom(1))
-            message_start_index = random.randint(0, 10000)
-
-            text = dataset[i]
-            text = text.replace('<br /><br />', ' ').replace('<br />', ' ')  # remove all '<br />'
-            text = ' '.join(sent_tokenize(text)[:1])  # Selecting leading 1 sentences as `context`
-            settings.seed = os.urandom(1)
-            example, sr = encode_speech(vocoder, tacotron, cmudict, message[message_start_index:], text, settings)
-
-            summary.add_example(example)
-            if save_data:
-                save_data_path = os.path.join(save_data_sub_dir, '{}.flac'.format(i))
-                write(os.path.join(save_data_path), sr, example.stego_object)
-                text_lst.append(text)
-        if save_data:
-            f_text.write('\n'.join(text_lst))
-
-        print(summary)
-        print()
-        df = pd.concat([df, summary.gather()], ignore_index=True)
-    save_table_dir = os.path.join('results', settings.task)
-    check_dir(save_table_dir)
-    save_table_filename = '{}_{}_{}.xlsx'.format(
-        settings.model_name.split('/')[-1],
-        str(settings.device).replace(':', '-'), time_stamp)
-    save_table_path = os.path.join(save_table_dir, save_table_filename)
-    df.to_excel(save_table_path)
-    if save_data:
-        f_text.close()
 
 
 if __name__ == '__main__':
-    # # Text Generation
-    settings = text_default_settings
-    settings.device = torch.device('cuda:0')
-    # settings.algo = 'Discop_baseline'
-    # settings.algo = 'sample'
-    get_text_statistics(settings, n_examples=10000, save_data=True)
+
 
     # # Image Completion
     settings = image_default_settings
@@ -315,9 +181,3 @@ if __name__ == '__main__':
     # settings.algo = 'sample'
     get_image_statistics(settings, n_examples=10000, save_data=True)
 
-    # Text-to-Speech
-    settings = audio_default_settings
-    settings.device = torch.device('cuda:0')
-    # settings.algo = 'Discop_baseline'
-    # settings.algo = 'sample'
-    get_audio_statistics(settings, n_examples=1000, save_data=True)
