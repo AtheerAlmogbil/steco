3ef8d46979c114ccd40c6704f90cd7d8b249ad58
AtheerAlmogbil <68163999+AtheerAlmogbil@users.noreply.github.com>
2024-02-03 08:51:49 -0500

    Update model.py

--- src/model.py
+++ src/model.py
@@ -1,3 +1,5 @@
+from transformers import GPT2Tokenizer, GPT2LMHeadModel
+from transformers import TransfoXLTokenizer, TransfoXLLMHeadModel
 from transformers import ImageGPTFeatureExtractor, ImageGPTForCausalImageModeling
 
 from transformers import PreTrainedTokenizer, PreTrainedModel
@@ -5,7 +7,14 @@ from config import Settings
 
 
 def get_model(settings: Settings) -> PreTrainedModel:
-    if settings.task == 'image':
+    if settings.task == 'text':
+        if settings.model_name in ['gpt2', 'distilgpt2']:
+            model = GPT2LMHeadModel.from_pretrained(settings.model_name).to(settings.device)
+        elif settings.model_name == 'transfo-xl-wt103':
+            model = TransfoXLLMHeadModel.from_pretrained(settings.model_name).to(settings.device)
+        else:
+            raise NotImplementedError
+    elif settings.task == 'image':
         if settings.model_name == 'openai/imagegpt-small':
             model = ImageGPTForCausalImageModeling.from_pretrained(settings.model_name).to(settings.device)
         else:
@@ -16,7 +25,15 @@ def get_model(settings: Settings) -> PreTrainedModel:
     return model
 
 
-
+def get_tokenizer(settings: Settings) -> PreTrainedTokenizer:
+    assert settings.task == 'text'
+    if settings.model_name in ['gpt2', 'distilgpt2']:
+        tokenizer = GPT2Tokenizer.from_pretrained(settings.model_name)  # local_files_only=True
+    elif settings.model_name == 'transfo-xl-wt103':
+        tokenizer = TransfoXLTokenizer.from_pretrained(settings.model_name)  # local_files_only=True
+    else:
+        raise NotImplementedError
+    return tokenizer
 
 
 def get_feature_extractor(settings: Settings) -> ImageGPTFeatureExtractor:
